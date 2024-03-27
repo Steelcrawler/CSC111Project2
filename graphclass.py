7baa60e41283fd816f1277f5e42cd66452017cf0
Steelcrawler <umailadi@gmail.com>
2024-03-26 23:28:14 -0400

    finished graph file

--- graphclass.py
+++ graphclass.py
@@ -98,30 +98,68 @@ def get_num_splits(attribute: str) -> int:
         return 3 # no instruments, some instruments, all instruments
 
 
-def Create_Attribute_Vertices(veritces: dict, attribute: str, attribute_interval: tuple) -> None:
+def Create_Attribute_Vertices(vertices: dict, attribute: str, attribute_interval: tuple) -> None:
     ''' Create a list of attribute vertices for a given attribute
     '''
     num_splits = get_num_splits(attribute)
     start = attribute_interval[0]
     end = attribute_interval[1]
+    interval_size = (end - start) / num_splits
     for i in range(num_splits):
-        interval = str(round(i * (end - start) / num_splits, 2)) + '-' + str(round((i + 1) * (end - start)  / num_splits, 2))
-        veritces[(attribute, interval)] = _Attribute_Vertex(attribute, interval, set())
+        interval_start = round(start + i * interval_size, 2)
+        interval_end = round(start + (i + 1) * interval_size, 2)
+        interval = f"{interval_start}-{interval_end}"
+        vertices[(attribute, interval)] = _Attribute_Vertex(attribute, interval, set())
 
 def get_value_range(value: float, total_range: tuple, num_splits: int) -> str:
-    ''' Get the range in which a given value falls
+    ''' Get the range in which a given value fallsm where the first number is inclusive and the second number is exclusive
     >>> get_value_range(0.25, (0.0, 1.0), 4)
-    
+    0.25-0.5
+    >>> get_value_range(0.0, (0.0, 1.0), 4)
+    0.0-0.25
+    >>> get_value_range(1.0, (0.0, 1.0), 4)
+    0.75-1.0
     '''
     start, end = total_range
     split_size = (end - start) / num_splits
     for i in range(num_splits):
-        range_start = start + i * split_size
-        range_end = start + (i + 1) * split_size
+        range_start = round(start + i * split_size, 2)
+        range_end = round(start + (i + 1) * split_size, 2)
         if range_start <= value < range_end:
             return f"{range_start}-{range_end}"
     return f"{end}-{end + split_size}"  # for the case when value equals to the end of total range
     
+
+class _Song_Vertex(_Vertex):
+    ''' A song vertex in a graph
+    '''
+    song_name: str
+    # artist: str
+    song_id: str
+
+    def __init__(self, song_name: str, song_id: str, neighbours: set[_Vertex]) -> None:
+        super().__init__(neighbours)
+        self.song_name = song_name
+        # self.artist = artist
+        self.song_id = song_id
+    
+    def add_neighbor(self, vertex: _Vertex) -> None:
+        ''' Add an edge between this song vertex and an attribute vertex
+        '''
+        self.neighbours.add(vertex)
+        vertex.neighbours.add(self)
+
+
+class _Attribute_Vertex(_Vertex):
+    ''' An attribute vertex in a graph
+    '''
+    attribute: str
+    interval: str
+
+    def __init__(self, attribute: str, interval: str, neighbours: set[_Vertex]) -> None:
+        super().__init__(neighbours)
+        self.attribute = attribute
+        self.interval = interval
 
 
 class _Song_Graph(Graph):
@@ -136,7 +174,7 @@ class _Song_Graph(Graph):
 
     def __init__(self) -> None:
         super().__init__()
-        self.attributes = {'valence': (0, 1), 'energy': (0, 1), 'danceability': (0, 1), 'loudness': (-60, 2),
+        self.attributes = {'valence': (0, 1), 'energy': (0, 1.25), 'danceability': (0, 1), 'loudness': (-60, 10),
                       'instrumentalness': (0, 1), 'tempo': (0, 250), 'speechiness':(0, 1)}
         for attribute in self.attributes:
             Create_Attribute_Vertices(self._vertices, attribute, self.attributes[attribute])
@@ -145,7 +183,7 @@ class _Song_Graph(Graph):
     def add_song(self, track_name, track_id, valence, energy, danceability, instrumentalness, tempo, speechiness, loudness):
         ''' Add a song to the graph by creating edges between the song and the attribute vertices
         '''
-        song_vertex = _Song_Vertex(track_name, track_id)
+        song_vertex = _Song_Vertex(track_name, track_id, set())
         song_attributes = {'valence': valence, 'energy': energy, 'danceability': danceability, 
                     'loudness': loudness, 'instrumentalness': instrumentalness, 
                     'tempo': tempo, 'speechiness': speechiness}
@@ -154,7 +192,10 @@ class _Song_Graph(Graph):
             start, end = self.attributes[attribute]
             num_splits = get_num_splits(attribute)
             range_str = get_value_range(song_attributes[attribute], (start, end), num_splits)
-            song_vertex.add_edge(self._vertices[(attribute, range_str)])
+            if (attribute, range_str) not in self._vertices:
+                print(f"Attribute vertex {attribute} {range_str} not found")
+            else:
+                song_vertex.add_neighbor(self._vertices[(attribute, range_str)])
     
     def read_csv_data(self, filename: str) -> None:
         ''' Read a csv file of song data and add the songs to the graph
@@ -170,28 +211,8 @@ class _Song_Graph(Graph):
     
 
 
-
-class _Song_Vertex(_Vertex):
-    ''' A song vertex in a graph
-    '''
-    song_name: str
-    artist: str
-    song_id: str
-
-    def __init__(self, song_name: str, artist: str, song_id: str, neighbours: set[_Vertex]) -> None:
-        super().__init__(neighbours)
-        self.song_name = song_name
-        self.artist = artist
-        self.song_id = song_id
-
-
-class _Attribute_Vertex(_Vertex):
-    ''' An attribute vertex in a graph
-    '''
-    attribute: str
-    interval: str
-
-    def __init__(self, attribute: str, interval: str, neighbours: set[_Vertex]) -> None:
-        super().__init__(neighbours)
-        self.attribute = attribute
-        self.interval = interval
+if __name__ == '__main__':
+    my_graph = _Song_Graph()
+    my_graph.read_csv_data('cleaned_spotify_songs.csv')
+    for vertex in my_graph._vertices[('valence', '0.25-0.5')].neighbours:
+        print(vertex.song_name)
