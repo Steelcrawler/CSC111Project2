041a16ffd150bdf546ad628dcad671fab3222d93
Steelcrawler <umailadi@gmail.com>
2024-04-04 10:31:14 -0400

    added comments

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,4 +1,8 @@
-"""CSC111 Project 2"""
+"""CSC111 Project 2
+
+File used to clean the spotify_songs.csv file. To run the cleaner, simply delete the else statement.
+Note: Running the cleaning process will override files currently in the directory.
+"""
 import doctest
 import pandas as pd
 import python_ta
@@ -14,12 +18,12 @@ else:
     df = pd.read_csv(file_path)
     columns_to_keep = ['track_artist', 'track_name', 'track_id', 'valence', 'energy',
                        'danceability', 'instrumentalness', 'tempo',
-                       'speechiness', 'loudness', 'track_popularity']
-    cleaned_df = df.dropna(subset=columns_to_keep)
+                       'speechiness', 'loudness', 'track_popularity']  # columns to keep from csv file
+    cleaned_df = df.dropna(subset=columns_to_keep)  # drops rows with missing data
     cleaned_df = cleaned_df[columns_to_keep]
     cleaned_df = cleaned_df.reset_index(drop=True)
     output_file_path = 'cleaned_spotify_songs.csv'
-    cleaned_df.to_csv(output_file_path, index=False)
+    cleaned_df.to_csv(output_file_path, index=False)  # saves the cleaned data
     summary_statistics = cleaned_df.describe()
-    output_file_path2 = 'summary_stats.csv'
+    output_file_path2 = 'summary_stats.csv'  #generates summary statistics for the data
     summary_statistics.to_csv(output_file_path2, index=False)
